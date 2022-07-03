ab177ebd9e65532e841f238ccf8b6d406d13ef9b
Julian Lanty <julianemanueltejo@gmail.com>
2022-07-03 07:19:32 -0300

    go to safe spot state

---
+++ Parcial 2/Assets/Scripts/Enemy/EnemyStates/GoToSafeSpot.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToSafeSpot<T> : State<T>
+{
+    
+    public override void Execute()
+    {
+        base.Execute();
+    }
+}
