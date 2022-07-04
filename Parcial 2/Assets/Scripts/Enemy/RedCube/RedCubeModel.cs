9b35151de08d2e6b7f6c58de54fba886e3a55272
Julian Lanty <julianemanueltejo@gmail.com>
2022-07-04 00:50:24 -0300

    implemented EnemyModel.cs  in RedCubeModel.cs

--- Parcial 2/Assets/Scripts/Enemy/RedCube/RedCubeModel.cs
+++ Parcial 2/Assets/Scripts/Enemy/RedCube/RedCubeModel.cs
@@ -4,30 +4,12 @@ using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-public class RedCubeModel : Entity
+public class RedCubeModel : EnemyModel
 {
-    public float rangeSight;
-    public float angleSight;
-    public LayerMask maskObstacles;
     [SerializeField]private float _damage = 15f;
-    int _lastFrameLOS;
-    bool _saveLOS;
-    Transform _lastTarget;
-
     public float DamageDelay = 10f;
     public float CurrentTime = 0f;
     private bool _hasDamaged;
-    public Damageable _damageable;
-    protected override void Awake()
-    {
-        base.Awake();
-    }
-
-    private void Start()
-    {
-        _damageable=GetComponent<Damageable>();
-    }
-
     private void Update()
     {
         if (!_hasDamaged) return;
@@ -38,49 +20,6 @@ public class RedCubeModel : Entity
             CurrentTime = 0f;
         }
     }
-
-    public bool LineOfSight(Transform target)
-    {
-        int currFrame = Time.frameCount;
-        if (_lastFrameLOS == currFrame && _lastTarget == target)
-        {
-            return _saveLOS;
-        }
-        _lastFrameLOS = currFrame;
-        _lastTarget = target;
-
-        var targetPos = target.position;
-        var pos = transform.position;
-        targetPos.y = 0;
-        pos.y = 0;
-
-        Vector3 diff = targetPos - pos;
-        float distance = diff.magnitude;
-
-        if (distance > rangeSight)
-        {
-            _saveLOS = false;
-            return _saveLOS;
-        } //Range
-
-        Vector3 dir = diff.normalized;
-        float angleToTarget = Vector3.Angle(dir, transform.forward);
-
-        if (angleToTarget > angleSight / 2)
-        {
-            _saveLOS = false;
-            return _saveLOS;
-        } //Angle
-
-        if (Physics.Raycast(transform.position, dir, distance, maskObstacles)) //View
-        {
-            _saveLOS = false;
-            return _saveLOS;
-        }
-        _saveLOS = true;
-        return _saveLOS;
-    }
-    
     private void OnCollisionEnter(Collision collision)
     {
         if (_hasDamaged) return;
@@ -103,14 +42,5 @@ public class RedCubeModel : Entity
             //GameManager.instance.Player._rb.AddForce(transform.forward * 100, ForceMode.Impulse);
             _hasDamaged = true;
         }
-    }
-    
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(transform.position, rangeSight);
-
-        Gizmos.DrawRay(transform.position, Quaternion.Euler(0, angleSight / 2, 0) * transform.forward * rangeSight);
-        Gizmos.DrawRay(transform.position, Quaternion.Euler(0, -angleSight / 2, 0) * transform.forward * rangeSight);
     }
 }
